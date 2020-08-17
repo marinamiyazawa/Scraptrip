ef28c12a2876e0a82a6dc39966f43f8b977674de
marinamiyazawa <mi.marina1030@gmail.com>
2020-08-17 17:19:19 +0900

    [fixed]user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,19 +14,19 @@ class UsersController < ApplicationController
       marker.infowindow post.title
     end
     # DMページ
-    @currentUserEntry = Entry.where(user_id: current_user.id)
-    @userEntry = Entry.where(user_id: @user.id)
+    @current_user_entry = Entry.where(user_id: current_user.id)
+    @user_entry = Entry.where(user_id: @user.id)
     if @user.id == current_user.id
     else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
+      @current_user_entry.each do |cu|
+        @user_entry.each do |u|
           if cu.room_id == u.room_id
-            @isRoom = true
-            @roomId = cu.room_id
+            @is_room = true
+            @room_id = cu.room_id
           end
         end
       end
-      if @isRoom
+      if @is_room
       else
         @room = Room.new
         @entry = Entry.new
